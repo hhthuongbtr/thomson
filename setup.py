cb334b0151b1da59e27b6eb2adfb01cf64e654d0
hhthuongbtr <hhthuongbtr@gmail.com>
2018-06-04 15:42:15 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Thomson",
+    version="0.0.1",
+    author="Thuong Huynh",
+    author_email="hhthuongbtr@gmail.com",
+    description="Thomson API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hhthuongbtr/thomson.git",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
